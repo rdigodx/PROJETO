d88a71ebcd261fbf7e94017f6efffdf9fe273f60
rdigo.dx <rodrigodigo11032005@gmail.com>
2026-04-14 19:47:56 -0300

    Sistema

--- sistema/estatisticas.py
+++ sistema/estatisticas.py
@@ -1,18 +1,19 @@
-
 def media_da_turma(lista_alunos):
+   
     soma_das_medias = 0
-    
     for cadastro in lista_alunos:
         media_aluno = sum(cadastro['notas']) / len(cadastro['notas'])
         soma_das_medias += media_aluno
-
     return soma_das_medias / len(lista_alunos)
 
 def melhor_aluno(lista_alunos):
-    for notas in enumerate(lista_alunos):
-        media = sum(notas) / len(notas)
+    melhor_media = 0 
+    
+    for cadastro in lista_alunos:
+        
+        media = sum(cadastro['notas']) / len(cadastro['notas'])
         
         if media > melhor_media:
             melhor_media = media
-    
+            
     return melhor_media
