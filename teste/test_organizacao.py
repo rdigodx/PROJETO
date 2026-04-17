4c44ad475ed757c4ec67d64e11fb619160dfd527
rdigodx <rodrigodigo11032005@gmail.com>
2026-04-17 20:09:33 -0300

    Commit teste

--- teste/test_organizacao.py
+++ teste/test_organizacao.py
@@ -1,14 +1,5 @@
 from sistema import organizacao
 
-
-def test_listar_alunos(lista_alunos_exemplo_valor_none):
-    listar = organizacao.listar_alunos(lista_alunos_exemplo_valor_none)
-    assert listar == lista_alunos_exemplo_valor_none
-
-def test_listar_alunos(lista_alunos_exemplo):
-    listar = organizacao.listar_alunos(lista_alunos_exemplo)
-    assert listar == lista_alunos_exemplo
-
 def test_separar_alunos(lista_alunos_exemplo):
     aprovados, reprovados = organizacao.separar_alunos(lista_alunos_exemplo)
 
